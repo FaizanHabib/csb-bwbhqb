cadba97b9d7a7c2de94be960246b2bcfa09ed0f4
Code Sandbox <hello@codesandbox.io>
2022-05-26 10:27:12 +0000

    Deployment from Codesandbox.

---
+++ index.js
@@ -0,0 +1,28 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+console.log(localStorage.clear("myLeads"));
+
+console.log(ulEl);
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  renderLeads();
+  inputEl.value = "";
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+      <li>
+          <a href='https://www.linkedin.com/in/faizanhabib/' target='_blank'>
+          ${myLeads[i]}
+          </a>
+      </li>
+      `;
+  }
+  ulEl.innerHTML = listItems;
+}
